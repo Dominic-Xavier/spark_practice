cd5da2986826b7f181e386f1aa1b8f75b7feb292
Dominic-Xavier <dominicxavier1@hotmail.com>
2026-01-18 14:59:26 +0530

    bucket and Key

--- src/utils/config_loader.py
+++ src/utils/config_loader.py
@@ -1,5 +1,5 @@
 import yaml
-import os
+import boto3
 from utils.path_utils import get_project_root
 
 def load_config(env: str):
@@ -14,9 +14,12 @@ def load_config(env: str):
     )"""
 
     file_path =  f"s3://employee-records-1001/Git-Code/config/{env}.yaml"
+    s3_path = file_path.replace("s3://", "")
+    bucket, key = s3_path.split("/", 1)
     if env == "prod":
-        with open(file_path, "r") as file:
-            config = yaml.safe_load(file)
+        s3_client = boto3.client("s3")
+        response = s3_client.get_object(Bucket=bucket, Key=key)
+        config = yaml.safe_load(response["Body"].read())
     elif env == "dev":
         with open(get_project_root() + f"\\config\\{env}.yaml", "r") as file:
             config = yaml.safe_load(file)
