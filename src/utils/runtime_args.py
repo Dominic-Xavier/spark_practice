832190e2b3ac5012c1679bec3ff7d1191311cadb
Dominic-Xavier <dominicxavier1@hotmail.com>
2026-01-18 13:51:14 +0530

    Glue Utils

--- src/utils/runtime_args.py
+++ src/utils/runtime_args.py
@@ -1,21 +1,33 @@
 import sys
 
 
+import sys
+
+
 def get_env_arg():
     """
-    Returns env argument in both local and Glue environments
+    Get --env argument in a way that works for:
+    - Local (python main.py --env dev)
+    - EMR (spark-submit main.py --env dev)
+    - AWS Glue (job parameter --env=prod)
     """
 
-    # Detect Glue runtime
-    if "awsglue" in sys.modules:
+    # ---------- AWS GLUE ----------
+    try:
         from awsglue.utils import getResolvedOptions
+
+        # If this import works, we ARE in Glue
         args = getResolvedOptions(sys.argv, ["env"])
         return args["env"]
 
-    # Local / EMR runtime
-    else:
+    except ImportError:
+        # ---------- LOCAL / EMR ----------
         import argparse
+
         parser = argparse.ArgumentParser()
         parser.add_argument("--env", required=True)
+
+        # IMPORTANT: parse_known_args, not parse_args
         args, _ = parser.parse_known_args()
         return args.env
+
