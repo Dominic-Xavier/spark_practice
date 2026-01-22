5ce7e511728443d8d04d015e36e369fa79dc5b5b
Dominic-Xavier <dominicxavier1@hotmail.com>
2026-01-23 02:20:19 +0530

    watermark path updated

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -5,14 +5,17 @@ def get_logger(name: str):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
 
-    # If no handlers exist, we're probably running locally
-    if not logger.handlers:
+    root_logger = logging.getLogger()
+
+    # Add handler ONLY if root logger has none (local env)
+    if not root_logger.handlers:
         handler = logging.StreamHandler(sys.stdout)
         formatter = logging.Formatter(
             "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
         )
         handler.setFormatter(formatter)
-        logger.addHandler(handler)
+        root_logger.addHandler(handler)
+        root_logger.setLevel(logging.INFO)
 
     logger.propagate = True
     return logger
